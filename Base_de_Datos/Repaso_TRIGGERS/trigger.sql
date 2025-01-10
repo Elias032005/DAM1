ea0a8bb91bcf7ef04ce06cd2e06ad5d5e7fe13c5
Elias <delgadooelias@gmail.com>
2025-01-10 10:58:39 +0100

    Update trigger.sql

--- Base_de_Datos/Repaso_TRIGGERS/trigger.sql
+++ Base_de_Datos/Repaso_TRIGGERS/trigger.sql
@@ -1,10 +1,12 @@
+
 USE tienda_deportes; -- Usamos la tabla con la que queremos que afecte el trigger
+
 /* Crea los siguientes triggers para las funciones:
 Trigger 1: Actualización Automática de Stock
-Trigger 2: Auditoría de Cambios en Productos
+Trigger 2: Auditoría de Cambios en Productos // update productos
 Trigger 3: Validación de Stock Negativo
 */
-
+-- TRIGGER 1
 DELIMITER //
 CREATE TRIGGER Act_auto_stock
 AFTER INSERT ON transacciones 
@@ -16,7 +18,7 @@ BEGIN
     -- la variable stock se actualizara dependiendo si el tipo de registro es de entrada o de salida
 		UPDATE productos SET stock = stock - NEW.cantidad WHERE id = NEW.producto_id;
         -- Usamos elconcat para concatenar diferentes tablas
-	
+
         ELSE IF NEW.tipo = 'ENTRADA'  THEN
 		UPDATE productos SET stock = stock + NEW.cantidad WHERE id = NEW.producto_id;
 		-- Usamos elconcat para concatenar diferentes tablas
@@ -24,5 +26,22 @@ BEGIN
         END IF;
         END IF;
 END;
+//
+
+-- TRIGGER 2
+
+DELIMITER // 
+CREATE TRIGGER Cambi_Produ
+AFTER INSERT ON productos
+FOR EACH ROW
+BEGIN
+	IF new.nombre THEN
+		UPDATE productos 
+		SET nuevo.nombre = NEW.nombre;
+		SET nueva.cantidad = NEW.stock;
+		SET nuevo.precio = NEW.precio
+	END IF;
+END
+
 //
 
