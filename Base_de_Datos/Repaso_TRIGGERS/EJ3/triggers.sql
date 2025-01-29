33a8df6d0f2ce33e828edaf6876ebf61fe43729c
Elias <delgadooelias@gmail.com>
2025-01-29 08:37:18 +0100

    Update triggers.sql

--- Base_de_Datos/Repaso_TRIGGERS/EJ3/triggers.sql
+++ Base_de_Datos/Repaso_TRIGGERS/EJ3/triggers.sql
@@ -1,4 +1,5 @@
 
+
 -- Cada vez que se agrega una evolución nyeva, se registra en la tabla auditría indicando que se ha aádido
 DELIMITER //
 CREATE TRIGGER Reg_evo
@@ -25,8 +26,8 @@ BEGIN
     DECLARE Num_habilidades INT;
 
     -- Obtener los tipos del Pokémon
-    SELECT tipo_primario, tipo_secundario INTO tipo_primario, tipo_secundario FROM Pokedex WHERE numero_dex = NEW.numero_dex;
-
+    SELECT tipo_primario, tipo_secundario INTO tipo_primario, tipo_secundario FROM pokedex WHERE numero_dex = (SELECT numero_dex FROM pokemon WHERE id = NEW.pokemon_id); 
+		-- El trigger coge el pokemon_id del registro que se está integrado en pokemon_movimientos (usando NEW.pokemon_id) y lo usa para obtener los tipos (tipo_primario y tipo_secundario) del Pokémon correspondiente en la tabla pokedex. No se crea ningún nuevo id, solo se recuperan los tipos del Pokémon con base en su pokemon_id ya existente.
     -- Obtener el tipo del movimiento
     SELECT tipo INTO tipo_movimiento FROM movimientos WHERE id = NEW.id;
 
@@ -44,6 +45,22 @@ BEGIN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'El Pokémon ya tiene 4 movimientos.';
     END IF;
+    
+    IF  EXISTS (SELECT 1 FROM Pokemon_movimientos WHERE pokemon_id = NEW.pokemon_id AND id = NEW.id ) THEN 
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Este movimiento ya lo conoce.';
+    END IF;
 END;
+//
 
+DELIMITER //
+CREATE TRIGGER Act_evoluciones
+AFTER UPDATE ON evoluciones
+FOR EACH ROW 
+BEGIN 
+	IF OLD.nivel_requerido = NEW.nivel.requerido THEN
+    INSERT INTO auditoria_movimientos (Id_evolucion,accion) VALUES
+    (NEW.Id, CONCAT('Nivel requerido acutalizado de: ', OLD.nivel_requerido ' a ', NEW.nivel_requerido));
+	END IF;
+END;
 //
